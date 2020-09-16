71754cb63890a7b20fcd24c94dbe53966175771b
Shakib448 <shakiba448@gmail.com>
2020-09-16 08:12:18 +0600

    React Travel Guru

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../Resources/Logo.png";
+import SearchIcon from "@material-ui/icons/Search";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto ml-auto">
+            <Link className="navbar-brand" to="/#">
+              <img src={logo} alt="" className="img-fluid" />
+            </Link>
+            <form className="form-inline my-2 my-lg-0">
+              <SearchIcon className="navbar__icon" />
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search Your Destination"
+                aria-label="Search"
+              />
+            </form>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/#">
+                News
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/#">
+                Destination
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/#">
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/#">
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/#">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCTzBUgRZ5JriHE9lUqvP2pyYJhQLVA-Ak",
+  authDomain: "react-travel-guru.firebaseapp.com",
+  databaseURL: "https://react-travel-guru.firebaseio.com",
+  projectId: "react-travel-guru",
+  storageBucket: "react-travel-guru.appspot.com",
+  messagingSenderId: "54964288202",
+  appId: "1:54964288202:web:daaaf010041af1abc21233",
+  measurementId: "G-CRJQM837DF",
+};
+
+export default firebaseConfig;
