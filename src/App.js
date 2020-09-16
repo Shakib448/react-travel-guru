fc0e62264aa2399bbe115e008a21d587d9213719
Shakib448 <shakiba448@gmail.com>
2020-09-16 08:57:51 +0600

    React Travel Guru

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Banner from "./Components/Banner/Banner";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Navbar />
+        <Banner />
       </Router>
     </div>
   );
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return <div className="banner"></div>;
+};
+
+export default Banner;
