7f8b6b953b34a851907dce3a9f630bb6b3beb65f
Shakib448 <shakiba448@gmail.com>
2020-09-16 21:57:56 +0600

    React Travel Grur

--- src/Components/BookingList/BookingList.js
+++ src/Components/BookingList/BookingList.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BookingList.css";
 import test from "../Resources/Image/Rectangle 27.png";
 import start from "../Resources/Icon/star_1_.png";
+import bookingListData from "../BookingListData/BookingListData";
 
 const BookingList = () => {
+  const [bookingList, setBookingList] = useState(bookingListData);
+  console.log(bookingList);
+
   return (
     <div className="bookDetails ">
       <div className="container ">
@@ -12,34 +16,44 @@ const BookingList = () => {
             <hr />
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-3">
-            <img src={test} className="bookingList__horizontalCard" alt="" />
-          </div>
-          <div className="col-md-3 bookingList__horizontalCardText">
-            <h6>Light bright airy stylish apt & safe peaceful stay</h6>
-            <div className="bookingList__details">
-              <p>4 guest 2 bedrooms 2 beds 2 baths</p>
-              <p>Wifi Air conditional Kitchen</p>
-              <p>Cancellation flexibility available</p>
-              <p>
-                <img src={start} alt="" className="bookingList__icon" />
-                <span className="bookingList__rating">4.9(20) </span>{" "}
-                <span className="bookingList__money">$34/night</span> $167total
-              </p>
+        <div className="row ">
+          {bookingList.map((booking) => (
+            <div className="row col-md-12 mb-4">
+              <div className="col-md-3">
+                <img
+                  src={test}
+                  className="bookingList__horizontalCard"
+                  alt=""
+                />
+              </div>
+              <div className="col-md-3 bookingList__horizontalCardText">
+                <h6>Light bright airy stylish apt & safe peaceful stay</h6>
+                <div className="bookingList__details">
+                  <p>4 guest 2 bedrooms 2 beds 2 baths</p>
+                  <p>Wifi Air conditional Kitchen</p>
+                  <p>Cancellation flexibility available</p>
+                  <p>
+                    <img src={start} alt="" className="bookingList__icon" />
+                    <span className="bookingList__rating">4.9(20) </span>{" "}
+                    <span className="bookingList__money">$34/night</span>{" "}
+                    $167total
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="col-md-6">
-            <iframe
-              width="100%"
-              height="495"
-              frameborder="0"
-              scrolling="no"
-              marginheight="0"
-              marginwidth="0"
-              src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"
-            ></iframe>
-          </div>
+          ))}
+        </div>
+
+        <div className="col-md-6 bookingList__googleMap">
+          <iframe
+            width="100%"
+            height="650"
+            frameBorder="0"
+            scrolling="no"
+            marginHeight="0"
+            marginWidth="0"
+            src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"
+          ></iframe>
         </div>
       </div>
     </div>
---
+++ src/Components/BookingListData/BookingListData.js
@@ -0,0 +1,35 @@
+import img1 from "../Resources/Image/Rectangle 26.png";
+import img2 from "../Resources/Image/Rectangle 27.png";
+import img3 from "../Resources/Image/Rectangle 28.png";
+
+const bookingListData = [
+  {
+    title: "Light bright airy stylish apt & safe peaceful stay",
+    img: img1,
+    desc1: "4 guest 2 bedrooms 2 beds 2 baths",
+    desc2: "Wifi Air conditional Kitchen",
+    desc3: "Cancellation flexibility available",
+    rating: "4.9(20)",
+    money: "34/night",
+  },
+  {
+    title: "Apartment in Lost Panorama",
+    img: img2,
+    desc1: "4 guest 2 bedrooms 2 beds 2 baths",
+    desc2: "Wifi Air conditional Kitchen",
+    desc3: "Cancellation flexibility available",
+    rating: "4.8(10)",
+    money: "52/night",
+  },
+  {
+    title: "AR Lounge & Pool (r&r + b+B)",
+    img: img3,
+    desc1: "4 guest 2 bedrooms 2 beds 2 baths",
+    desc2: "Wifi Air conditional Kitchen",
+    desc3: "Cancellation flexibility available",
+    rating: "4.9(25)",
+    money: "44/night",
+  },
+];
+
+export default bookingListData;
