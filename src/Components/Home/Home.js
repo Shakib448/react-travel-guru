29e8523eaf0869af6869812521600cc0518d39e0
Shakib448 <shakiba448@gmail.com>
2020-09-17 08:37:53 +0600

    Rect Travel Guru

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,27 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5 home__content ">
+            <h1>Coxes Bazar</h1>
+            <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              voluptatum eum sit, similique ut provident ex, quo rem, sint
+              accusamus dolorem. Obcaecati tempore quia a totam laboriosam.
+              Asperiores cumque provident officia molestias ex doloribus,
+              obcaecati accusamus explicabo, assumenda nam facilis, voluptatum
+              maxime alias quis fugit natus dolores? Veniam architecto, nesciunt
+              tenetur corporis amet ratione repudiandae et neque labore fugit
+              dolorem quo blanditiis voluptates vero magnam quam, unde
+            </h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
